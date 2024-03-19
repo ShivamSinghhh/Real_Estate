208e61c244c0b0e03373183ddf8241a137e21ec5
Shivam Singh <91656460+ShivamSinghhh@users.noreply.github.com>
2024-03-19 17:24:12 +0530

    created register api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,27 @@
+const bcrypt = require("bcrypt");
+const { UserModel } = require("../models/User.model.js");
 const getUsers = (req, res) => {
-  res.send({ msg: "All users details.." });
+  try {
+    res.send({ msg: "All users details.." });
+  } catch (error) {
+    res.send({ msg: error.message });
+  }
 };
 
-module.exports ={getUsers}
+const signupHandler = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashPassword = bcrypt.hashSync(password, 5);
+  const lowerCaseEmail = email.toLowerCase();
+  try {
+    await new UserModel({
+      username,
+      email: lowerCaseEmail,
+      password: hashPassword,
+    }).save();
+    res.send({ msg: "user added successfully..!!" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+module.exports = { getUsers, signupHandler };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const { UserModel } = require("../models/User.model");
-const {getUsers} = require("../controllers/user.controller")
+const {getUsers, signupHandler} = require("../controllers/user.controller.js");
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.post("/adduser", async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    await UserModel({ username, email, password }).save();
-    res.send({ msg: "User added successfully" });
-  } catch (error) {
-    console.log("error adding user", error);
-  }
-});
+userRouter.post("/register", signupHandler);
 
 module.exports = { userRouter };
